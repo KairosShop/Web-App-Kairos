9441c33c48c640d91f54748cddb1d34d76499940
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-02 17:19:44 -0500

    type (#26) feat create modules initial

---
+++ src/app/comparation/comparation.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ComparationRoutingModule } from './comparation-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ComparationRoutingModule
+  ]
+})
+export class ComparationModule { }
