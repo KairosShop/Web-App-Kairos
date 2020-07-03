cbf35e8b6737d85be0f2a4d68da36e88e54d3dfc
Armando <rivera.armando997@gmail.com>
2020-07-02 22:40:11 -0500

    style (#46) create top navbar without button

---
+++ src/app/shared/components/top-navbar/top-navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-navbar',
+  templateUrl: './top-navbar.component.html',
+  styleUrls: ['./top-navbar.component.scss']
+})
+export class TopNavbarComponent implements OnInit {
+
+	public homepage: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
