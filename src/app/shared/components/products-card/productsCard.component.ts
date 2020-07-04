5e680ad507653222a701988e2241a5f4c6b464e7
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-03 21:47:15 -0500

    style [#48] implement style card horizontal

--- src/app/shared/components/products-card/productsCard.component.ts
+++ src/app/shared/components/products-card/productsCard.component.ts
@@ -6,28 +6,42 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./productsCard.component.scss']
 })
 
-export class ProductsCardComponent implements OnInit {
+export class ProductsCardComponent implements OnInit  {
   @Input() type: string;
+  @Input() direction: string;
   @Input() product: {
-    id:any,
-    img:any,
-    name:any,
-    weight:any,
-    description:any,
-    cound:any,
-    price:number,
-    super1:any,
-    super2:any,
-    super3:any,
+    id: any,
+    img: any,
+    name: any,
+    weight: any,
+    description: any,
+    cound: any,
+    price: number,
+    super1: {
+      id: any,
+      name: any,
+      logo: any,
+      price: any
+    },
+    super2: {
+      id: any,
+      name: any,
+      logo: any,
+      price: any
+    },
+    super3: {
+      id: any,
+      name: any,
+      logo: any,
+      price: any
+    },
   };
 
   constructor() {
 
   }
 
-  
   ngOnInit(): void {
-    console.log(this.type)
   }
 
 }
