c692cd26367aaaa8121ce9354b0e22fab4e8678e
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-03 23:47:15 -0500

    feat[#65] create route module home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+/* index */
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 /* Modules */
-
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from '@core/core.module';
 import { HomeModule } from './home/home.module';
+
+/* component */
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    HomeModule
+    CoreModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
