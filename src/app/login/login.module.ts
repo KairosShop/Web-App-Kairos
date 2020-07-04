a20c520c7da44973e0cd4eea31e45f21cf6351ce
Armando <rivera.armando997@gmail.com>
2020-07-04 02:54:00 -0500

    style (#58) form register style

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,15 +4,16 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from '../shared/shared.module';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
     SharedModule
   ],
-  exports: [LoginComponent]
+  exports: [LoginComponent, RegisterComponent]
 })
 export class LoginModule { }
