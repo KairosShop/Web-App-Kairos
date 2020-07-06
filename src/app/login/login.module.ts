dfddc478fa81075a8d7ed2ab36fcac68ce30268f
Armando <rivera.armando997@gmail.com>
2020-07-05 20:50:55 -0500

    fix (#58) throw new error in api-request and login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -18,6 +19,7 @@ import { RegisterComponent } from './components/register/register.component';
     LoginRoutingModule,
     SharedModule,
     RouterModule,
+    ReactiveFormsModule
   ],
   exports: [
     RouterModule,
