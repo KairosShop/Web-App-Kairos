ad2a63d0980c0c6a7231aeed9955fd40a37c504a
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-05 21:35:38 -0500

    feat [#65] created component garden products

---
+++ src/app/shared/garden-products/garden-products.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-garden-products',
+  templateUrl: './garden-products.component.html',
+  styleUrls: ['./garden-products.component.scss']
+})
+export class GardenProductsComponent implements OnInit {
+  @Input() title = 'Title';
+  @Input() products = [
+    {
+      "id": 26,
+      "title": "Espinaca baby Marketside",
+      "quantity": 250,
+      "id_measure": 2,
+      "description": "Marketside es una manera saludable de mantener en forma tu salud y bienestar.",
+      "id_category": 2,
+      "id_subcategory": 19,
+      "url_image": "https://res.cloudinary.com/walmart-labs/image/upload/w_960,dpr_auto,f_auto,q_auto:best/gr/images/product-images/img_large/00750179166766L.jpg",
+      "active": 1
+    },
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
