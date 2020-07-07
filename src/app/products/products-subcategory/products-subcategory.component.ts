5e8ebb07f84253413b34e08a6f47f64a3990ccfd
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-06 23:40:49 -0500

    feat [#49] init modules products

---
+++ src/app/products/products-subcategory/products-subcategory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-subcategory',
+  templateUrl: './products-subcategory.component.html',
+  styleUrls: ['./products-subcategory.component.scss']
+})
+export class ProductsSubcategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 
+/* component */
 import { ProductsRoutingModule } from './products-routing.module';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductsCategoryComponent } from './products-category/products-category.component';
+import { ProductsSubcategoryComponent } from './products-subcategory/products-subcategory.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductDetailComponent,
+    ProductsCategoryComponent,
+    ProductsSubcategoryComponent
+  ],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    SharedModule
   ]
 })
 export class ProductsModule { }
