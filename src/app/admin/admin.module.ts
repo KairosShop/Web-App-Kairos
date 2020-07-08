37808801663c2cb2433b41fa61671ea87ee02f6d
Armando <rivera.armando997@gmail.com>
2020-07-08 14:24:50 -0500

    feat (#67) import module shared

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { PanelComponent } from './components/panel/panel.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PanelComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    SharedModule
   ]
 })
 export class AdminModule { }
