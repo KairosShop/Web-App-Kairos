dd5d8e9899374bd3291c487108a516d275016eea
Armando <rivera.armando997@gmail.com>
2020-07-09 03:22:51 -0500

    feat (#54) gurad for user profile

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
 
+import { AuthGuard } from './auth.guard';
+
 const routes: Routes = [
 	{
-		path: '',
+		path: '', canActivate: [ AuthGuard ],
 		component: ProfileComponent
 	}
 ];
