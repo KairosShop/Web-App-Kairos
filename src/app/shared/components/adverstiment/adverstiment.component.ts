1bfd4e554e8d569fe980187b8f43755f54b2d990
Armando <rivera.armando997@gmail.com>
2020-07-10 17:14:04 -0500

    styles (56) supermarket add

---
+++ src/app/shared/components/adverstiment/adverstiment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adverstiment',
+  templateUrl: './adverstiment.component.html',
+  styleUrls: ['./adverstiment.component.scss']
+})
+export class AdverstimentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
