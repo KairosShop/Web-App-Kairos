80e7ac9eb8c37c61024fee3e8e0b89bd4359a40f
Armando <rivera.armando997@gmail.com>
2020-07-11 14:16:54 -0500

    feat (#55) count plus and minus in cards

--- src/app/shared/components/cart/cart.component.ts
+++ src/app/shared/components/cart/cart.component.ts
@@ -17,25 +17,31 @@ export class CartComponent implements OnInit {
 
   constructor(
     private _addCartService: AddCartService
-  ) {}
+  ) {
+    console.log('cart component');
+    this.count = this.loadCart(this._addCartService.getCart());
+  }
 
   ngOnInit(): void {
     this._addCartService
       .getObservable()
       .subscribe((response: Product[]) => {
          
-         this.count = response
-                      .map((product:Product) => {
-                         return product.count;
-                       })
-                      .reduce((count, value)=> {
-                          return count + value;
-                      }, 0);
-
-        this.cart = response;
+        this.count = this.loadCart(response);
         this.animation = true;
         setTimeout(() => this.animation=false, 800);
       });
+  }
+
+  loadCart(response):number {
+    this.cart = response;
+    return response
+      .map((product:Product) => {
+         return product.count;
+       })
+      .reduce((count, value)=> {
+          return count + value;
+      }, 0);
   }
 
 }
