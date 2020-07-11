2ae931bf0c496ca9cdd68f69414d5a0517d04ef8
Armando <rivera.armando997@gmail.com>
2020-07-11 02:30:25 -0500

    styles (#56) adverstiments

--- src/app/core/authentication/auth.service.ts
+++ src/app/core/authentication/auth.service.ts
@@ -58,7 +58,7 @@ export class AuthService {
   getCookie(name) {
     let nameEQ = name + "=";
     let ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
+    for (let i = 0; i < ca.length; i++) {
       let c = ca[i];
       while (c.charAt(0) === " ") c = c.substring(1, c.length);
       if (c.indexOf(nameEQ) === 0) return JSON.parse(c.substring(nameEQ.length, c.length));
