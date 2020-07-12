d965c18937fe94a9c5c43b6bc235787dafb64729
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-11 21:39:05 -0500

    feat [#51] created of component all to one

--- src/app/comparation/comparation-routing.module.ts
+++ src/app/comparation/comparation-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ComparationComponent } from './components/comparation/comparation.component';
+import { AllInOneComponent } from './components/all-in-one/all-in-one.component';
 
 
 const routes: Routes = [
 	{
 		path: '',
 		component: ComparationComponent
+	},
+	{
+		path: '/all-in-one',
+		component: AllInOneComponent
 	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class ComparationRoutingModule { }
