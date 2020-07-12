3e4f1f00f83a4c8e840a7f54b28297bd60d15976
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-07-12 14:35:10 -0500

    add (#74) test home component

--- src/app/home/components/home/home.component.spec.ts
+++ src/app/home/components/home/home.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { CategoriesService } from '@core/categories/categories.service';
+import { ProductsService } from '@core/products/products.service';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('HomeComponent', () => {
+class ProductsServiceStub {
+  getProductsOfCategories() {
+    let products = ['product'];
+    return of(products);
+  }
+
+}
+
+class CategoriesStub {
+  getAllCategories() {
+    let categories = [{category:'productCategory',id:1}];
+    return of(categories);
+  }
+}
+  
+
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        { provide: CategoriesService, useClass:CategoriesStub },
+        { provide: ProductsService, useClass: ProductsServiceStub },
+      ],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
