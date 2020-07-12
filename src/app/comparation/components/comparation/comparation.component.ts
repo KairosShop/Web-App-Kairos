7f60e2a85098cf8718dc1c36a6b163515e32f16a
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-11 21:51:55 -0500

    fix [#51] type

--- src/app/comparation/components/comparation/comparation.component.ts
+++ src/app/comparation/components/comparation/comparation.component.ts
@@ -9,7 +9,77 @@ import { ApiRequestsService } from '@core/apiRequest/api-requests.service';
 export class ComparationComponent implements OnInit {
 
   @Input() cart: string = '0001';
+  headers = [
+    {
+      id: 2,
+      supermarket: "Crown Crafts, Inc.",
+      urlImage: "http://dummyimage.com/229x104.png/dddddd/000000",
+      all: {
+        total_products: 10,
+        total_sum: 636
+      },
+      cheapest: {
+        total_products: 10,
+        total_sum: 636
+      },
+    },
+    {
+      id: 2,
+      supermarket: "Super2",
+      urlImage: "http://dummyimage.com/229x104.png/dddddd/000000",
+      all: {
+        total_products: 8,
+        total_sum: 637
+      },
+      cheapest: {
+        total_products: 2,
+        total_sum: 65
+      },
+    },
+    {
+      id: 3,
+      supermarket: "SUPER 3",
+      urlImage: "http://dummyimage.com/229x104.png/dddddd/000000",
+      all: {
+        total_products: 10,
+        total_sum: 636
+      },
+      cheapest: {
+        total_products: 5,
+        total_sum: 63
+      },
+    },
+  ]
+  products = [
+    {
+      id: 5,
+      name: 'papitas',
+      urlImage: 'http://dummyimage.com/229x104.png/dddddd/000000',
+      supermarkers: [
+        {
+          idSupermerkert: 1,
+          name: 'Super1',
+          urlImage: 'http://dummyimage.com/229x104.png/dddddd/000000',
+          price: 10
+        },
+        {
+          idSupermerkert: 1,
+          name: 'Super2',
+          urlImage: 'http://dummyimage.com/229x104.png/dddddd/000000',
+          price: 10
+        },
+        {
+          idSupermerkert: 1,
+          name: 'Super3',
+          urlImage: 'http://dummyimage.com/229x104.png/dddddd/000000',
+          price: 10
+        },
+      ]
+    },
+  ]
 
+  totalProducts: number;
+  totalValue: number;
 
   constructor(
     private apiRequestService: ApiRequestsService
