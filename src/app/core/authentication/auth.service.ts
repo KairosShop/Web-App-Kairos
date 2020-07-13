52b04d90470f774c210fd8192afe0923bac78509
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-12 23:54:57 -0500

    milisegunos cookie

--- src/app/core/authentication/auth.service.ts
+++ src/app/core/authentication/auth.service.ts
@@ -53,7 +53,7 @@ export class AuthService {
     let expires = "";
     if (days) {
       let date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+      date.setTime(date.getTime() + 90000);
       expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Lax;";
