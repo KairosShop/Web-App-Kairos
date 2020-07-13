23d5c1933717323bbcce30c72abe920c1609ebce
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-12 20:33:21 -0500

    fix[#78] error app.module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from "./AppComponent";
+import { AppComponent } from "./app.component";
 import { By } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
