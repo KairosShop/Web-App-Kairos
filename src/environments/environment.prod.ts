7908c52371b02d8817293110f8f13be30eebee14
Alejandro López Ramírez <alejozepol@gmail.com>
2020-07-13 00:31:32 -0500

    fix url microservicio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,18 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA5U9xbfZB5MRagJpJEfKgEsYpOhz8waOE",
+  authDomain: "kairos-282000.firebaseapp.com",
+  databaseURL: "https://kairos-282000.firebaseio.com",
+  projectId: "kairos-282000",
+  storageBucket: "kairos-282000.appspot.com",
+  messagingSenderId: "408189019549",
+  appId: "1:408189019549:web:d05f01b40e81d5e50ebb74",
+  measurementId: "G-Q1226DJKQ1"
+};
+
 export const environment = {
   production: true,
   mapboxKey: 'pk.eyJ1IjoiYXJtYW5kby1yaXZlcmEiLCJhIjoiY2tidmc0eHh5MDVxZjMycWZpenByMWtmZiJ9.yOC42dTFZzhulcp7o3vyNA',
-  URL_API: 'https://api.kairosshop.xyz/api'
+  URL_API: 'https://api.kairosshop.xyz/api',
+  firebaseConfig: firebaseConfig,
+  URL_MICROSERVICE: 'https://flask-demo-282117.ue.r.appspot.com/comparator'
 };
